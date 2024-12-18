126354c786bde7631c2eb1099fa405275aee0629
Neesui <nanumaya2012@gmail.com>
2024-12-18 22:16:27 +0545

    update code App.jsx and add component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';  // Ensure this file exists in the src directory
 import Navbar from './Components/Navbar.jsx'
 
 function App() {
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-
 const Navbar = () => {
   return (
     <>
-    <div>
-        <div className='bg-white'>
-            <H1 className='text-2xl font-bold'>Job Portal App</H1>
+      <div>
+        <div className='bg-black'>
+          <h1 className='text-2xl font-bold'>Job Portal App</h1>
         </div>
         <div>
-            <ul>
-                <li>Home</li>
-                <li>Jobs</li>
-                <li>Browser</li>
-            </ul>
+          <ul>
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browser</li>
+          </ul>
         </div>
-    </div>
+      </div>
     </>
   )
 }
